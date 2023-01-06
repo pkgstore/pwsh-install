d5fd5e6b4b03503defb74dce06b9aa1b69cc9044
KitsuneSolar <kitsune.solar@gmail.com>
2023-01-06 23:16:30 +0300

    2023-01-06 20:16:30

---
+++ PkgStore.Install.psd1
@@ -0,0 +1,13 @@
+@{
+  RootModule = 'PkgStore.Install.psm1'
+  ModuleVersion = '1.0.0'
+  GUID = ''
+  Author = 'Kitsune Solar'
+  CompanyName = 'v77 Development'
+  Copyright = '(c) 2023 v77 Development. All rights reserved.'
+  Description = ''
+  PowerShellVersion = '7.1'
+  FunctionsToExport = @('')
+  CmdletsToExport = @()
+  AliasesToExport = @()
+}
---
+++ PkgStore.Install.psm1
@@ -0,0 +1,60 @@
+function Install-PowerShell() {
+  <#
+    .SYNOPSIS
+      Download and install PowerShell.
+
+    .DESCRIPTION
+
+    .PARAMETER Version
+      PowerShell version.
+      Alias: '-V'.
+
+    .PARAMETER Type
+      Package type.
+      Default: 'zip'.
+      Alias: '-T'.
+
+    .EXAMPLE
+      Install-PowerShell -V '7.2.8' -T 'zip'
+
+    .EXAMPLE
+      Install-PowerShell -V '7.2.8' -T 'msi'
+
+    .LINK
+      Package Store: https://github.com/pkgstore
+
+    .NOTES
+      Author: Kitsune Solar <mail@kitsune.solar>
+  #>
+
+  [CmdletBinding()]
+
+  Param(
+    [Parameter(Mandatory)]
+    [ValidatePattern('^((\d+)\.(\d+)\.(\d+))$')]
+    [Alias('V')]
+    [string]${Version},
+
+    [ValidateSet('msi', 'zip')]
+    [Alias('T')]
+    [string]${Type} = 'zip'
+  )
+
+  ${URL_DL} = "https://github.com/PowerShell/PowerShell/releases/download"
+  ${D_APPS} = "${env:SystemDrive}\Apps"
+  ${D_PWSH} = "${D_APPS}\PowerShell"
+  ${F_PWSH} = "PowerShell-${Version}-win-x64.${Type}"
+
+  New-Item -Path "${D_APPS}" -ItemType "Directory" -Force
+  Invoke-WebRequest "${URL_DL}/v${Version}/${F_PWSH}" -OutFile "${D_APPS}\${F_PWSH}"
+
+  if ( "${Type}" -eq 'zip' ) {
+    if ( Test-Path -Path "${D_PWSH}" ) { Remove-Item -Path "${D_PWSH}" -Recurse -Force }
+    Expand-Archive -Path "${D_APPS}\${F_PWSH}" -DestinationPath "${D_PWSH}"
+    Remove-Item -Path "${D_APPS}\${F_PWSH}";
+  } elseif ( "${Type}" -eq 'msi' ) {
+    msiexec.exe /package "${D_APPS}\${F_PWSH}" /quiet ENABLE_PSREMOTING=1 REGISTER_MANIFEST=1 USE_MU=1 ENABLE_MU=1 ADD_PATH=1
+  } else {
+    Write-Error -Message "Package type not found!" -ErrorAction "Stop"
+  }
+}
