3e76a61a6042afaaf5a8f013cd998af0a750ebf1
KitsuneSolar <kitsune.solar@gmail.com>
2023-01-06 23:56:36 +0300

    2023-01-06 20:56:36

--- PkgStore.Install.psm1
+++ PkgStore.Install.psm1
@@ -45,14 +45,19 @@ function Install-PowerShell() {
   ${D_PWSH} = "${D_APPS}\PowerShell"
   ${F_PWSH} = "PowerShell-${Version}-win-x64.${Type}"
 
+  Write-Information -MessageData "--- Create directory: '${D_APPS}'..."
   New-Item -Path "${D_APPS}" -ItemType "Directory" -Force
+
+  Write-Information -MessageData "--- Download PowerShell: '${F_PWSH}'..."
   Invoke-WebRequest "${URL_DL}/v${Version}/${F_PWSH}" -OutFile "${D_APPS}\${F_PWSH}"
 
   if ( "${Type}" -eq 'zip' ) {
     if ( Test-Path -Path "${D_PWSH}" ) { Remove-Item -Path "${D_PWSH}" -Recurse -Force }
+    Write-Information -MessageData "--- Expand: '${F_PWSH}'"
     Expand-Archive -Path "${D_APPS}\${F_PWSH}" -DestinationPath "${D_PWSH}"
     Remove-Item -Path "${D_APPS}\${F_PWSH}";
   } elseif ( "${Type}" -eq 'msi' ) {
+    Write-Information -MessageData "--- Install: '${F_PWSH}'"
     msiexec.exe /package "${D_APPS}\${F_PWSH}" /quiet ENABLE_PSREMOTING=1 REGISTER_MANIFEST=1 USE_MU=1 ENABLE_MU=1 ADD_PATH=1
   } else {
     Write-Error -Message "Package type not found!" -ErrorAction "Stop"
